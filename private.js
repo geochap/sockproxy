52f5112e638c81cce8edd5a05eb8235d677390a9
Geoff Chappell <geoff@sover.net>
2012-10-10 09:26:42 -0400

    initial commit

---
+++ private.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var io = require('socket.io-client');
+var querystring = require('querystring');
+
+var remote = 'http://www.example.org:2000';
+
+var socket = io.connect(remote);
+
+socket.on('connect', function () {
+  console.log('connect');
+});
+socket.on('status', function (data) {
+  console.log(data);
+});
+socket.on('disconnect', function () {
+  console.log('disconnect');
+});
+socket.on('request', function (req) {
+  forwardRequest(req);
+});
+
+
+function forwardRequest(desc) {
+
+  var options = {
+    host: 'localhost',
+    port: 3100,
+    path: desc.url,
+    method: desc.method
+  };
+
+  var req = http.request(options, function (res) {
+    var content = [];
+    res
+        .on('end', function () {
+          socket.emit('response', {
+            id: desc.id,
+            responseText: Buffer.concat(content).toString('base64'),
+            statusCode: res.statusCode,
+            headers: res.headers
+          });
+        })
+        .on('data', function (chunk) {
+          content.push(chunk);
+        });
+  });
+
+  req.end();
+}
+
---
+++ public.js
@@ -0,0 +1,75 @@
+var express = require('express')
+  , http = require('http');
+
+var io = require('socket.io');
+
+var app = express();
+
+var _requestid = 0;
+var _cbs = {};
+var _socket;
+
+app.configure(function () {
+  app.set('port', process.env.PORT || 3200);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function () {
+  app.use(express.errorHandler());
+});
+
+app.get('*', function (req, res, next) {
+  proxyRequest(req, function (err, data) {
+    if (err)
+      sendError(err);
+    else {
+      res.statusCode = data.statusCode;
+      for (var key in data.headers)
+        res.setHeader(key, data.headers[key]);
+
+      res.send(new Buffer(data.responseText, 'base64'));
+    }
+  });
+});
+
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+io = io.listen(server);
+
+io.sockets.on('connection', function (socket) {
+  _socket = socket;
+
+  socket.emit('status', { status: 'ok' });
+  socket.on('response', function (data) {
+    if (_cbs[data.id]) {
+      var cb = _cbs[data.id];
+      delete _cbs[data.id];
+      cb(null, data);
+    }
+    else {
+      console.log('unknown request');
+    }
+  });
+});
+
+function proxyRequest(req, cb) {
+  var request = {
+    id:_requestid++,
+    method: req.method,
+    url: req.url,
+    headers: req.headers
+  };
+  
+  _cbs[request.id] = cb;
+
+  _socket.emit('request', request);
+}
